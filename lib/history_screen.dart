b7b4e21c1f846f8e8948b86f6e99006839ab43bb
“yonemori” <yonemori950@gmail.com>
2025-06-23 11:59:58 +0900

    広告実装

--- lib/history_screen.dart
+++ lib/history_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'database_helper.dart';
 
 class HistoryScreen extends StatefulWidget {
@@ -9,11 +10,42 @@ class HistoryScreen extends StatefulWidget {
 class _HistoryScreenState extends State<HistoryScreen> {
   final dbHelper = DatabaseHelper();
   List<Map<String, dynamic>> _expenses = [];
+  
+  // AdMobバナー広告
+  BannerAd? _bannerAd;
+  bool _isAdLoaded = false;
 
   @override
   void initState() {
     super.initState();
     _loadExpenses();
+    _loadBannerAd();
+  }
+
+  void _loadBannerAd() {
+    _bannerAd = BannerAd(
+      adUnitId: 'ca-app-pub-8148356110096114/3236336102',
+      size: AdSize.banner,
+      request: AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _isAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          print('Ad failed to load: ' + error.toString());
+          ad.dispose();
+        },
+      ),
+    );
+    _bannerAd!.load();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
   }
 
   Future<void> _loadExpenses() async {
@@ -35,36 +67,49 @@ class _HistoryScreenState extends State<HistoryScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('家計簿履歴')),
-      body: _expenses.isEmpty
-          ? Center(child: Text('まだ登録されていません'))
-          : ListView.builder(
-              itemCount: _expenses.length,
-              itemBuilder: (context, index) {
-                final item = _expenses[index];
-                return Dismissible(
-                  key: ValueKey(item['id']),
-                  direction: DismissDirection.endToStart,
-                  background: Container(
-                    color: Colors.red,
-                    alignment: Alignment.centerRight,
-                    padding: EdgeInsets.only(right: 20),
-                    child: Icon(Icons.delete, color: Colors.white),
+      body: Column(
+        children: [
+          Expanded(
+            child: _expenses.isEmpty
+                ? Center(child: Text('まだ登録されていません'))
+                : ListView.builder(
+                    itemCount: _expenses.length,
+                    itemBuilder: (context, index) {
+                      final item = _expenses[index];
+                      return Dismissible(
+                        key: ValueKey(item['id']),
+                        direction: DismissDirection.endToStart,
+                        background: Container(
+                          color: Colors.red,
+                          alignment: Alignment.centerRight,
+                          padding: EdgeInsets.only(right: 20),
+                          child: Icon(Icons.delete, color: Colors.white),
+                        ),
+                        onDismissed: (_) => _deleteExpense(item['id']),
+                        child: Card(
+                          margin: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                          child: ListTile(
+                            title: Text('💴 ${item['amount']}'),
+                            subtitle: Text('📅 ${item['date']}　🏪 ${item['store']}'),
+                            trailing: IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () => _deleteExpense(item['id']),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                  onDismissed: (_) => _deleteExpense(item['id']),
-                  child: Card(
-                    margin: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                    child: ListTile(
-                      title: Text('💴 ${item['amount']}'),
-                      subtitle: Text('📅 ${item['date']}　🏪 ${item['store']}'),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () => _deleteExpense(item['id']),
-                      ),
-                    ),
-                  ),
-                );
-              },
+          ),
+          // バナー広告
+          if (_isAdLoaded)
+            Container(
+              width: _bannerAd!.size.width.toDouble(),
+              height: _bannerAd!.size.height.toDouble(),
+              child: AdWidget(ad: _bannerAd!),
             ),
+        ],
+      ),
     );
   }
 } 
